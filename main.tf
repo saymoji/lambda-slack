37c1bd8bf521f445f8e7b9129a2af85e2b55172f
nalbam <me@nalbam.com>
2018-06-12 12:19:02 +0900

    tf-deploy

---
+++ main.tf
@@ -0,0 +1,22 @@
+# Terraform Main
+
+module "demo-slack" {
+  source = "git::https://github.com/nalbam/terraform-aws-lambda-sns.git"
+  region = "${var.region}"
+
+  name        = "${var.name}"
+  stage       = "${var.stage}"
+  description = "sns > lambda > slack"
+  runtime     = "nodejs8.10"
+  handler     = "index.handler"
+  memory_size = 512
+  timeout     = 5
+  s3_bucket    = "${var.s3_bucket}"
+  s3_source    = "target/lambda.zip"
+  s3_key       = "lambda/${var.name}/${var.name}-${var.version}.zip"
+
+  env_vars = {
+    PROFILE = "${var.stage}"
+    SLACK_HOOK_URL = "${var.SLACK_HOOK_URL}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable region {
+  default = "ap-northeast-2"
+}
+
+variable name {
+  default = "demo-slack"
+}
+
+variable stage {
+  default = "dev"
+}
+
+variable domain {
+  default = "saymoji.com"
+}
+
+variable s3_bucket {
+  default = "repo.saymoji.com"
+}
+
+variable version {
+  default = "1.0.0"
+}
+
+variable SLACK_HOOK_URL {
+  default = ""
+}
