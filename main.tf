0a3951b379a5a365bd2c1402bf962552cfaf33c8
nalbam <me@nalbam.com>
2018-08-16 12:18:29 +0900

    circleci/node:10

--- main.tf
+++ main.tf
@@ -1,5 +1,17 @@
 # Terraform Main
 
+provider "aws" {
+  region = "${var.region}"
+}
+
+terraform {
+  backend "s3" {
+    region = "ap-northeast-2"
+    bucket = "terraform-nalbam-seoul"
+    key = "demo-slack.tfstate"
+  }
+}
+
 module "demo-slack" {
   source = "git::https://github.com/nalbam/terraform-aws-lambda-sns.git"
   region = "${var.region}"
